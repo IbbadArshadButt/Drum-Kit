973ecbbf99d9ec8ed6f480410dbf26159573fa74
Ibad Butt <135251611+IbbadArshadButt@users.noreply.github.com>
2024-08-31 23:10:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,37 +18,37 @@ function makeSound(key){
     switch (key) {
         
             case "d":
-                var crash = new Audio('sounds/crash.mp3');
+                var crash = new Audio('crash.mp3');
                 crash.play();
                 break;
     
             case "r":
-                var kick = new Audio('sounds/kick-bass.mp3');
+                var kick = new Audio('kick-bass.mp3');
                 kick.play();
                 break;
     
             case "u":
-                var snare = new Audio('sounds/snare.mp3');
+                var snare = new Audio('snare.mp3');
                 snare.play();
                 break;
     
             case "m":
-                var tom1 = new Audio('sounds/tom-1.mp3');
+                var tom1 = new Audio('tom-1.mp3');
                 tom1.play();
                 break;
     
             case "k":
-                var tom2 = new Audio('sounds/tom-2.mp3');
+                var tom2 = new Audio('tom-2.mp3');
                 tom2.play();
                 break;
     
             case "i":
-                var tom3 = new Audio('sounds/tom-3.mp3');
+                var tom3 = new Audio('tom-3.mp3');
                 tom3.play();
                 break;
     
             case "t":
-                var tom4 = new Audio('sounds/tom-4.mp3');
+                var tom4 = new Audio('tom-4.mp3');
                 tom4.play();
                 break;
     
